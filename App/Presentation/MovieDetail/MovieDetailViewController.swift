e2394c9fcf0ec8662ba0008a0ea56b47d984a15a
Daniel de Souza Ribas <danielribas@tokenlab.com.br>
2022-06-02 12:11:27 -0300

    feat: create movie detail view controller

---
+++ App/Presentation/MovieDetail/MovieDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDetailViewController.swift
+//  App
+//
+//  Created by Daniel de Souza Ribas on 01/06/22.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
