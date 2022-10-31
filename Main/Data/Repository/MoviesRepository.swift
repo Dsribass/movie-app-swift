dbb9b0bbd7baf2e1e043b14819345266f75138d2
Daniel Ribas <ds_ribas@hotmail.com>
2022-10-31 18:33:34 -0300

    feat: add movies repository protocol in domain

--- Main/Data/Repository/MoviesRepository.swift
+++ Main/Data/Repository/MoviesRepository.swift
@@ -9,7 +9,7 @@ import Foundation
 import RxSwift
 import Domain
 
-class MoviesRepository {
+class MoviesRepository: MoviesRepositoryProtocol {
   init(
     movieRDS: MovieRemoteDataSource,
     userPreferencesCDS: UserPreferencesCacheDataSource
