3ae90c41d82dc76f63539c287aa9d17b864cc6e2
Daniel Ribas <ds_ribas@hotmail.com>
2022-11-09 10:35:45 -0300

    test: setup presentation tests

---
+++ PresentationTests/GeneratedMocks.swift
@@ -0,0 +1,13 @@
+// MARK: - Mocks generated from file: Domain/UseCases/Movies/GetMovieSummaryList.swift at 2022-11-09 13:34:56 +0000
+
+//
+//  GetMovieSummaryList.swift
+//  Domain
+//
+//  Created by Daniel de Souza Ribas on 31/10/22.
+//
+
+import Cuckoo
+@testable import Domain
+
+import RxSwift
